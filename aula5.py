3f4ac29c87bc5119d877ca283fc20ef6e83090ab
mansano03 <biancamanssno11647@gmail.com>
2025-02-12 15:33:50 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,78 @@
+# 1 
+print('Sejam bem vindos')
+
+
+# 2 
+
+booleana =  True
+tipo = type(booleana)
+print(tipo)
+
+
+#3
+
+multi =  (5.5 * 4.9) + 20 
+print(multi)
+
+
+# 4
+
+div = 10/2
+print(div)
+
+
+# 5 
+
+print(10-2)
+
+# 6 
+
+
+print(10/2)
+
+
+# 7 
+
+numeros  =  4* 2* 3 * 2
+print(numeros)
+
+
+# 8 
+um número inteiro. Em seguida, 
+
+
+numero_int = 10
+print(numero_int ** 2)
+
+
+# 9 
+n1 = float(input('Digite um número:'))
+n2 = float(input('Digite um número:'))
+
+soma =  n1 + n2
+subtracao = n1 -n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+print(f'soma: {soma} | subtração: {subtracao} | multiplicação: {multiplicacao} | divisão {divisao}   ')
+
+
+
+
+# 10 
+
+
+nome  =  input('Digite seu nome: ')
+idade  =  int(input('Digite sua idade: '))
+email =  input('Digite seu e-mail: ')
+endereco = input('Digite seu endereço')
+
+print('..........................................')
+print('DADOS CADASTRADOS:')
+
+print(f''' 
+NOME: {nome}
+IDADE: {idade}
+E-MAIL:{email}
+ENDEREÇO: {endereco}
+''')
